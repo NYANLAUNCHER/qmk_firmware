9867668d94996b0c1430113b60991763a6697529
markiep <purcellmarkie@proton.me>
2025-11-20 23:21:50 -0600

    Added keyboards/nyanlaucher

---
+++ keyboards/nyanlauncher/charybdis/3x5_3/kb2040/readme.md
@@ -0,0 +1,2 @@
+# Adafruit KB2040
+This is the configuration for the Charybids 3x5+3 using Adafruit's kb2040 microcontroller.
---
+++ keyboards/nyanlauncher/charybdis/3x5_3/keymaps/default/keymap.h
@@ -0,0 +1,15 @@
+// vim: filetype=c
+#pragma once
+#include QMK_KEYBOARD_H
+
+#define LAYOUT_TRNS LAYOUT( \
+_______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, \
+_______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, \
+_______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, \
+                  _______, _______, _______,   _______, _______, _______ )
+
+#define LAYOUT_NOOP LAYOUT( \
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+                  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX )
